ff8a57130e872b3b6ba517243a3b4281084bcfe6
Inderjeet Singh <87217817+inderjeet549@users.noreply.github.com>
2025-02-13 19:15:13 +0530

    Create ques15.js

---
+++ CUSDM02S1_2507_S03_&_S04: ES6 & Execution Context/ques15.js
@@ -0,0 +1,11 @@
+let age = 25; 
+function displayAge() {
+    console.log("Age in displayAge:", age);
+}
+function changeAge() {
+    age = 30;
+    console.log("Age after changeAge:", age);
+}
+displayAge();
+changeAge();
+displayAge();
