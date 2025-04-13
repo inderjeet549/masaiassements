c62bc0cb47f86624438031190859adf7fef8f843
Inderjeet Singh <87217817+inderjeet549@users.noreply.github.com>
2025-04-13 17:55:48 +0530

    Create firebase.js

---
+++ CUSDM04S3_2515: Authentication\Authorization using Firebase/ques 1/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBc0dK8i10rHktJ2Oq7uHhBOw-z-5BiGGQ",
+  authDomain: "massi-assessment.firebaseapp.com",
+  databaseURL: "https://massi-assessment-default-rtdb.firebaseio.com",
+  projectId: "massi-assessment",
+  storageBucket: "massi-assessment.firebasestorage.app",
+  messagingSenderId: "276722086538",
+  appId: "1:276722086538:web:ada7102c941aaedfd5f373",
+  measurementId: "G-HZEH0VX5M5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
